058614393751b897121d97e04cca989439216aa6
SantanaDiogoDev <diogo.santana.dev@gmail.com>
2023-11-07 09:02:55 -0300

    Fetching backend data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from 'axios';
+// import './home.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    const [restaurants, setRestaurants] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchRestaurants = async () => {
+            try {
+                const response = await axios.post('http://localhost:8080/restaurants/filter', {
+                    params: {
+                        name: 'yummy',
+                        customerRating: '3',
+                        distance: '15',
+                        price: '35',
+                        cuisine: ''
+                    }
+                });
+                setRestaurants(response.data);
+            } catch (error) {
+                console.error('Error fetching restaurants: ', error);
+            }
+        };
+        fetchRestaurants();
+    }, []);
+
+    return(
+      <div>
+        <h1>Teste</h1>
+        <div>
+          {restaurants.map(restaurant =>(
+              <div key={restaurant.name}>
+                  <h2>{restaurant.name}</h2>
+                  <p>{restaurant.customerRating}</p>
+              </div>
+          ))}
+        </div>
+      </div>
+    );
 }
 
 export default App;
